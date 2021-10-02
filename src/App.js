9deee084c629eb92f4e449c4e4353b64e68b2128
Abdullah Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2021-10-02 11:17:15 +0300

    Token contract created and deployed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import Token from './artifacts/contracts/Token.sol/Token.json'
 
 // Update with the contract address logged out to the CLI when it was deployed 
 // const greeterAddress = "<YOUR_CONTRACT_ADDRESS>"
-const greeterAddress = "0xe98587C08e712Bf491F545ecA42A4ab36A18650a"
+// const tokenAddress = "<YOUR_CONTRACT_ADDRESS>"
 
 function App() {
   // store greeting in local state
   const [greeting, setGreetingValue] = useState("")
+  const [userAccount, setUserAccount] = useState()
+  const [amount, setAmount] = useState()
 
   // request access to the user's MetaMask account
   async function requestAccount() {
@@ -46,8 +49,27 @@ function App() {
     }
   }
 
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
 
-  console.log({ greeting })
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transation = await contract.transfer(userAccount, amount);
+      await transation.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
 
   return (
     <div className="App">
@@ -55,6 +77,12 @@ function App() {
         <button onClick={fetchGreeting}>Fetch Greeting</button>
         <button onClick={setGreeting}>Set Greeting</button>
         <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+
+        <br />
+        <button onClick={getBalance}>Get Balance</button>
+        <button onClick={sendCoins}>Send Coins</button>
+        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
       </header>
     </div>
   );
